ea44f18db0f07da165f304edb0bb4a2e22cd422f
josiah <mainajosiahdev@gmail.com>
2018-08-26 17:20:46 -0400

    comitting the user interface

--- js/master.js
+++ js/master.js
@@ -1,23 +1,3 @@
-var randomNumber
-var marks=[]
-var totalscore
-function roll(){
-    var randomNumber=Math.floor((Math.random()*6)+1)
-    // userinterface
-    $(document).ready(function(){
-      $("#randomResult1").text(randomNumber)
-      $("#totalresult1").text(totalscore)
-    })
-    if(randomNumber !==1){
-      marks.push(randomNumber)
-      totalscore= marks.reduce((a,b)=>a+b)
-    }
-    else {
-      marks.length=0
-      totalscore=0
-    }
-}
-
-function hold(){
-
-}
+var randomNumber;
+var marks=[];
+var totalscore;
