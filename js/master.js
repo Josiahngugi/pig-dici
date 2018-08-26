d375ab8e256ced1297fce985cf29c48ef439a69d
josiah <mainajosiahdev@gmail.com>
2018-08-26 18:33:28 -0400

    commiting roll and hold constructs

--- js/master.js
+++ js/master.js
@@ -1,3 +1,51 @@
-var randomNumber;
-var marks=[];
-var totalscore;
+var randomNumber,randomNumber2;
+var marks1=[];
+var marks2=[];
+var totalscore, totalscore2;
+var finalscore
+var prayer1 , prayer2
+ function roll1(){
+   var randomNumber=Math.floor((Math.random()*6)+1);
+  if (randomNumber!==1){
+      marks1.push(randomNumber)
+      var totalscore=marks1.reduce((a,b)=>a+b)
+    }
+    else{
+      var totalscore=marks.length=0
+
+    }
+
+    if (totalscore>=100||finalscore>=100){
+      alert("Hooray you are winner")
+    }
+    $(document).ready(function(){
+        $("#randomResult").text(randomNumber)
+        $("#totalresult").text(totalscore)
+    });
+
+ }
+
+function hold(){
+  var totalscore= finalscore
+}
+// user interface
+function roll2(){
+  var randomNumber2=Math.floor((Math.random()*6)+1);
+   if (randomNumber2!==1){
+     marks2.push(randomNumber2)
+     var totalscore2=marks2.reduce((a,b)=>a+b)
+   }
+   else{
+     var totalscore=marks2.length=0
+
+   }
+
+   if (totalscore2>=100||finalscore>=100){
+     alert("Hooray you are winner")
+   }
+   $(document).ready(function(){
+       $("#randomResult2").text(randomNumber2)
+       $("#totalresult2").text(totalscore2)
+   });
+
+}
